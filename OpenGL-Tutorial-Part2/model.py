13365ebad018bc793eae771929b18f52f3ae52d7
Pelops2 <42607409+austinsw@users.noreply.github.com>
2022-11-29 17:21:10 +0800

    Completion of Transformation (Scaling)

--- OpenGL-Tutorial-Part2/model.py
+++ OpenGL-Tutorial-Part2/model.py
@@ -3,10 +3,11 @@ import numpy as np
 import glm
 
 class BaseModel:
-    def __init__(self, app, vao_name, tex_id, pos=(0, 0, 0), rot=(0, 0, 0)):
+    def __init__(self, app, vao_name, tex_id, pos=(0, 0, 0), rot=(0, 0, 0), scale=(1, 1, 1)):
         self.app = app
         self.pos = pos
         self.rot = glm.vec3([glm.radians(a) for a in rot])
+        self.scale = scale
         self.m_model = self.get_model_matrix()
         self.tex_id = tex_id
         self.vao = app.mesh.vao.vaos[vao_name]
@@ -23,6 +24,8 @@ class BaseModel:
         m_model = glm.rotate(m_model, self.rot.x, glm.vec3(1, 0, 0))
         m_model = glm.rotate(m_model, self.rot.y, glm.vec3(0, 1, 0))
         m_model = glm.rotate(m_model, self.rot.z, glm.vec3(0, 0, 1))
+        # scale
+        m_model = glm.scale(m_model, self.scale)
         return m_model
 
     def render(self):
@@ -30,8 +33,8 @@ class BaseModel:
         self.vao.render()
 
 class Cube(BaseModel):
-    def __init__(self, app, vao_name='cube', tex_id=0, pos=(0, 0, 0), rot=(0, 0, 0)):
-        super().__init__(app, vao_name, tex_id, pos, rot)
+    def __init__(self, app, vao_name='cube', tex_id=0, pos=(0, 0, 0), rot=(0, 0, 0), scale=(1, 1, 1)):
+        super().__init__(app, vao_name, tex_id, pos, rot, scale)
         self.on_init()
 
     def update(self):
